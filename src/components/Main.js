8fc20243c5dfb6f218a851244df321196d85d434
Jesus Ramirez <jrmz.maldonado@gmail.com>
2020-11-25 11:21:38 -0600

    Main component created and styled

---
+++ src/components/Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Main() {
+    return (
+        <div className="main">
+            <div className="main--logo"></div>
+            <div className="main--txt">
+                <h1>The Star Wars SPA gives you the basic information on the first six movies, their main characters, and plantes.</h1>
+                <h2>All this was made with 💛, lots of ☕, and React</h2>
+                <p>Created and designed by <a href="https://jesusrmz.com/" target="_blank" rel="noreferrer">Jesús</a></p>
+            </div>
+        </div>
+    );
+}
+
+export default Main;
