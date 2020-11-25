2a37fd10ab901611fe99de9ebf34e9b50dfe018f
Jesus Ramirez <jrmz.maldonado@gmail.com>
2020-11-25 11:23:05 -0600

    rel att added to a tag component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,15 +4,15 @@ import MenuItem from './MenuItem';
 class Navbar extends React.Component {
       render() {
           return (
-        <navbar className="navbar__container">
+        <div className="navbar__container">
           <div className="logo"></div>
           <div className="navbar">
               <MenuItem tagline="Movies"/>
               <MenuItem tagline="Characters"/>
               <MenuItem tagline="Planets"/>
           </div>
-          <p className="thanks">This is example is possible thanks to the <a href="https://swapi.dev/api/" target="_blank" >Star Wars API</a> 💛</p>
-        </navbar>
+          <p className="thanks">This project was possible thanks to the <a href="https://swapi.dev/api/" target="_blank" rel="noreferrer">Star Wars API</a> 💛</p>
+        </div>
       );
     }
     
