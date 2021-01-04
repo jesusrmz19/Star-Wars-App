3b3c8b2a6caa37f114fd8c269aca6b5211169292
Jesus Ramirez <jrmz.maldonado@gmail.com>
2021-01-03 20:15:38 -0600

    Vehicles component connect to the API with one example

--- src/components/Vehicle.js
+++ src/components/Vehicle.js
@@ -2,20 +2,21 @@ import React from 'react';
 
 class Vehicle extends React.Component {
     render() {
+        const { name, crew, cost_in_credits, max_atmosphering_speed, passengers, vehicle_class} = this.props.details;
         return(
             <div className="vehicle">
                 <div className="back empire">
                     <img className="back--img" src="/images/starships/starwars_light.svg" alt="starwars-logo"></img>
                 </div>
                 <div className="front">
-                    <img className="front--img" src="#" alt="test"></img>
+                    <img className="front--img" src="#" alt={name}></img>
                     <div className="front--txt--wrapper">
-                        <p className="name"></p>
-                        <p className="cost">cred.</p>
-                        <p className="charac">MAX SPEED<span>10</span></p>
-                        <p className="charac">HYPERDRIVE RATING<span>10</span></p>
-                        <p className="charac">CREW<span>10</span></p>
-                        <p className="charac">PASSENGERS<span>10</span></p>
+                        <p className="name">{name}</p>
+                        <p className="cost">{cost_in_credits} cred.</p>
+                        <p className="charac">MAX SPEED<span>{max_atmosphering_speed}</span></p>
+                        <p className="charac">VEHICLE CLASS<span>{vehicle_class}</span></p>
+                        <p className="charac">CREW<span>{crew}</span></p>
+                        <p className="charac">PASSENGERS<span>{passengers}</span></p>
                     </div>
                 </div>
             </div>
--- src/components/Vehicles.js
+++ src/components/Vehicles.js
@@ -2,17 +2,70 @@ import React from 'react';
 import Vehicle from './Vehicle';
 
 class Vehicles extends React.Component {
+    state = {
+        error: null,
+        isLoaded: false,
+        vehicles: [],
+        vehiclesArr: [],
+        index: 0,
+        page: null
+    };
+
+    _isMounted = false;
+
+    componentDidMount() {
+        this._isMounted = true;
+        this.fetchVehicle(7);
+    }
+
+    componentWillUnmount() {
+        this._isMounted = false;
+    }
+
+    fetchVehicle = async(page) => {
+        this.setState({
+            page
+        });
+        try {
+            const response = await fetch(`https://swapi.dev/api/vehicles/${page}/`);
+            const data = await response.json();
+            const vehicles = data;
+            if(this._isMounted) {
+                this.setState({
+                    vehicles,
+                    isLoaded: true
+                });
+            }
+        }catch(error){
+            if(!this._isMounted){
+                this.setState({
+                    isLoaded: true,
+                    error
+                });
+            }
+        }
+    }
+
     render() {
-        return(
-            <div className="vehicles--container">
-                <h1>Vehicles</h1>
-                <div className="vehicles">
-                    <button className="vehicles--btn btnV--prev"></button>
-                    <Vehicle />
-                    <button className="vehicles--btn btnV--next"></button>
+        const { vehicles, error, isLoaded } = this.state;
+        if(error) {
+            return (<div className="vehicles--container"><div className="loading">Error</div></div>);
+        } else if (!isLoaded) {
+            return (<div className="vehicles--container"><div className="loading">Loading...</div></div>);
+        } else {
+            return(
+                <div className="vehicles--container">
+                    <h1>Vehicles</h1>
+                    <div className="vehicles">
+                        <button className="vehicles--btn btnV--prev"></button>
+                        <Vehicle
+                            details = {vehicles}
+                        />
+                        <button className="vehicles--btn btnV--next"></button>
+                    </div>
                 </div>
-            </div>
-        );
+            );
+        }
     }
 }
 
